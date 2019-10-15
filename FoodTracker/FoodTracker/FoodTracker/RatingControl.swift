929269d6e5946c7b18ae367e1d0f0a5fe0515054
Kabelo <33831896+kmashao@users.noreply.github.com>
2019-10-15 18:08:29 +0200

    Add files via upload
    added tap gesture recognition

---
+++ FoodTracker/FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Kabelo MASHAO on 2019/10/15.
+//  Copyright © 2019 Kabelo MASHAO. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+	//Mark: Initialization
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+
+}
